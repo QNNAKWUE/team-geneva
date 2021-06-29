baf920789d35b42ae427b439bc4599e855dd0769
Queen Nnakwue <qnnakwue@gmail.com>
2021-06-29 09:28:32 +0100

    making updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import {getWeatherData} from './data/weatherapi';
 import {ScaleLoader} from 'react-spinners';
 
